bd3134ae92bd52aacc6492d15ba7a926a8d917e9
Botsa Poojitha <poojithabotsa0@gmail.com>
2026-06-18 16:56:37 +0530

    Update traffic_light_controller.ino

--- 04_Traffic_Light_Controller/traffic_light_controller.ino
+++ 04_Traffic_Light_Controller/traffic_light_controller.ino
@@ -1,1 +1,28 @@
+// Define the pin numbers for each LED
+int red = 10;
+int yellow = 8;
+int green = 6;
+void setup() {
+  // Set the LED pins as outputs
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+}
+void loop() {
+  // 1. GREEN light on for 5 seconds
+  digitalWrite(green, HIGH);
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, LOW);
+  delay(6000);
+  // 2. YELLOW light on for 2 seconds
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, HIGH);
+  digitalWrite(red, LOW);
+  delay(2000);
+  // 3. RED light on for 5 seconds
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, HIGH);
+  delay(5000);
+}
 
